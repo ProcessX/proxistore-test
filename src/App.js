03140f06ef3209767257539c70090bc116318287
ProcessX <luc2526@gmail.com>
2021-03-11 11:49:12 +0100

    React Router Installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Switch, Route} from 'react-router-dom';
 
 const App = () => {
-  return (
-    <>
-      <h1>Welcome to React Parcel Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-    </>
-  );
+  // return (
+    
+  //   <>
+  //     <h1>Welcome to React Parcel Micro App!</h1>
+  //     <p>Hard to get more minimal than this React app.</p>
+  //   </>
+  // );
+  
+  return <Switch>
+    <Route exact path="/">
+      <h1>This is Home page</h1>
+    </Route>
+  </Switch>
 };
 
 export default App;
--- src/test.js
+++
@@ -1,3 +0,0 @@
-const test = 9
-
-console.log(test)
