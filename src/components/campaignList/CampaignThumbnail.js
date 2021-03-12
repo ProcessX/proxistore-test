9cecbb87fcc1449a40ac42c5984f9c264703f478
ProcessX <luc2526@gmail.com>
2021-03-12 12:01:19 +0100

    Campaign Listing - All elements present

--- src/components/campaignList/CampaignThumbnail.js
+++ src/components/campaignList/CampaignThumbnail.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ThumbnailOptions from './ThumbnailOptions'
 
 const CampaignThumbnail = () => {
     return (
         <Link className={'campaignThumbnail'}>
             <p className={'campaign__status'}>Status</p>
-            <h3 className={'campaign__name'}>Campaign's name</h3>
-            <p className={'campaign__id'}>Campaign's ID</p>
+            <h3 className={'campaign__name'}>Campaign&apos;s name</h3>
+            <p className={'campaign__id'}>
+                43746d72-6117-4778-a953-1b376a44eac0
+            </p>
+            <ThumbnailOptions />
         </Link>
     )
 }
